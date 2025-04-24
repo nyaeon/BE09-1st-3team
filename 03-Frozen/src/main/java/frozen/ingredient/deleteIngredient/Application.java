9a47a1cedf38f6a14811a89c359019c5b5c3a44c
nayoung_lee <skdud0694@gmail.com>
2025-04-24 16:28:10 +0900

    병합 과정 #2

--- 03-Frozen/src/main/java/frozen/ingredient/deleteIngredient/Application.java
+++ 03-Frozen/src/main/java/frozen/ingredient/deleteIngredient/Application.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+/*        Scanner sc = new Scanner(System.in);
         System.out.print("삭제하실 식재료의 이름을 입력해주세요 : ");
         String delete = sc.nextLine();
         System.out.println("삭제할 식재료의 수령을 입력해주세요 : ");
@@ -21,7 +21,7 @@ public class Application {
 
         Management management = new Management(LocalDate.now(),checkDelete,deleteAmount,delete);
         managemenntService managementService = new managemenntService();
-//        managementService.updateDelete(menagement);
+//        managementService.updateDelete(menagement);*/
 
     }
 }
