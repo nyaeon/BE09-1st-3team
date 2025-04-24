9424ea70e4b295668ecff2b9aff1ae76a396a857
nayeon <nyaooe@gmail.com>
2025-04-24 10:39:24 +0900

    관리자 레시피 등록 작업 중

---
+++ 03-Frozen/src/main/java/frozen/admin/dto/AdminDTO.java
@@ -0,0 +1,59 @@
+package frozen.admin.dto;
+
+public class AdminDTO {
+
+
+    private String menuName;
+    private String ingredients;
+    private String method;
+    private String time;
+    private int level;
+
+    public AdminDTO() {
+        this.menuName = menuName;
+        this.ingredients = ingredients;
+        this.method = method;
+        this.time = time;
+        this.level = level;
+    }
+
+    public String getMenuName() {
+        return menuName;
+    }
+
+    public void setMenuName(String menuName) {
+        this.menuName = menuName;
+    }
+
+    public String getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(String ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+}
